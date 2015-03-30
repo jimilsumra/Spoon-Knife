46e38051c7ba9031ef69c50ffdc24c81dbd63d25
Jimil <jimil@logicore.co.nz>
2015-03-30 14:56:16 +1300

    Hi I have created aboutme.php file please have a look with uppercase changes.

---
+++ aboutme.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jimmy
+ * Date: 30/03/2015
+ * Time: 2:52 PM
+ */
+
+
+    echo "Hello this is about us page";
+    $name = "Jimil S";
+    $name = ucfirst($name);
+    echo "Welcome my world. ".$name;
